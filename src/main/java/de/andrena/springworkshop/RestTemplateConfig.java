4f8631379966433b2c38fbb72121621e693abfca
Bonnie Chow <bonnie.chow@andrena.de>
2017-09-28 17:10:45 +0200

    initial commit

---
+++ src/main/java/de/andrena/springworkshop/RestTemplateConfig.java
@@ -0,0 +1,30 @@
+package de.andrena.springworkshop;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.client.Traverson;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@Configuration
+public class RestTemplateConfig {
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+//	@Bean
+//	public Traverson traverson(){
+//		UriComponentsBuilder uriBuilder = UriComponentsBuilder.newInstance();
+//		URI uri = uriBuilder.scheme("http").host("localhost").port("8090").build().toUri();
+//		Traverson traverson = new Traverson(uri, MediaTypes.HAL_JSON);
+//		traverson.setRestOperations(restTemplate());
+//		return traverson;
+//	}
+
+
+}
---
+++ src/main/java/de/andrena/springworkshop/dao/EventDaoImpl.java
@@ -0,0 +1,68 @@
+package de.andrena.springworkshop.dao;
+
+import de.andrena.springworkshop.dto.EventDTO;
+import de.andrena.springworkshop.dto.SpeakerDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.Resources;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class EventDaoImpl implements EventDao {
+
+    private final RestTemplate restTemplate;
+
+    private String scheme = "http";
+    private String host = "localhost:8090";
+
+    @Autowired
+    public EventDaoImpl(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+
+    @Override
+    public List<EventDTO> getAllEvents() {
+        final String path = "/events";
+        final UriComponentsBuilder apiUrlBuilder = UriComponentsBuilder.newInstance();
+        final String url = apiUrlBuilder.scheme(scheme).host(host).path(path).build().toString();
+
+        Resources<Resource<EventDTO>> eventResponse = sendRequest(url, new ParameterizedTypeReference<Resources<Resource<EventDTO>>>() {
+        });
+
+        return mapToDTOList(eventResponse);
+    }
+
+    private List<EventDTO> mapToDTOList(Resources<Resource<EventDTO>> eventResponse) {
+        return eventResponse.getContent().stream().map(Resource::getContent).collect(Collectors.toList());
+    }
+
+    private <T> T sendRequest(String url, ParameterizedTypeReference<T> type) {
+        ResponseEntity<T> response = restTemplate.exchange(url, HttpMethod.GET, null, type);
+        if (response != null) {
+            return response.getBody();
+        }
+        return null;
+    }
+
+    @Override
+    public List<EventDTO> getEventsWithTitleContaining(String title) {
+        //not yet implemented :(
+        return null;
+    }
+
+    @Override
+    public List<EventDTO> getEventsWithDescriptionContaining(String description) {
+        //not yet implemented :(
+        return null;
+    }
+
+}
